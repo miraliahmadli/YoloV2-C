231987fee25a2bf4bdf2d6da2ce590567d33c65e
Mirali <miraliahmadli@gmail.com>
2020-06-04 19:52:43 +0000

    Almost done Pthread

--- src/dnn_avx.c
+++ src/dnn_avx.c
@@ -3,103 +3,159 @@
 #include <string.h>
 #include <assert.h>
 #include <pthread.h>
+#include<unistd.h> 
+#include<stdlib.h> 
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#define BLOCK 169
+double *A_p;
+double *B_p;
+int M, N, K;
 
+void *mult(void* arg)
+{
+    int *data = (int *)arg;
+    double sum = 0.0;
+    int i = 0;
 
-void conv2d(double *C, double *A, double *B, int M, int K, int N){
-    // M x K, K x N -> M x N
+    int row = *data;
+    int col = *(data + 1);
+    for (i = 0; i < K; i++){
+        sum += A_p[row * K + i] * B_p[i * N + col];
+        // if(row == 0 && col == 30000)
+        //     printf("PTHREAD %f %d %f\n",sum, i, A_p[row * K + i] * B_p[i * N + col]);
+    }
+    double *p = (double*)malloc(sizeof(double));
+    *p = sum;
+    free(arg);
+    pthread_exit(p);
+}
 
-    // for(int i = 0; i < N; i++){
-    //     for(int j = 0; j < K; j++){
-    //         B[i + j*N] = B[j + i*K];
-    //     }
-    // }
+void *mult2(void* arg)
+{
+    int *data = (int *)arg;
+    double sum = 0.0;
+    int i = 0, j =0;
 
-    // __m256d col, row, res;
-    // double store_res[4];
-    // for(int i = 0; i < M; i++){
-    //     for(int j = 0; j < N; j++){
-    //         double sum = 0.0;
-    //         for(int k = 0; k < K; k = k + 4){
-    //             col = _mm256_loadu_pd(&A[k + i*K]);
-    //             row = _mm256_loadu_pd(&B[k + j*K]);
-    //             // int a_offset = k + i*K;
-    //             // int b_offset = k + j*K;
-    //             // int b_offset = j + k*N;
-    //             // col = _mm256_set_pd(A[a_offset], A[a_offset + 1], A[a_offset + 2], A[a_offset + 3]);
-    //             // row = _mm256_set_pd(B[b_offset], B[b_offset + k], B[b_offset + 2*k], B[b_offset + 3*k]);
-    //             res = _mm256_mul_pd(col, row);
-    //             // _mm256_storeu_pd(store_res, res);
-    //             sum += (*(double *)&res[0]) + (*(double *)&res[1]) + 
-    //                     (*(double *)&res[2]) + (*(double *)&res[3]);
-    //         }
+    int row = *data;
+    int col = *(data + 1);
+    double p[BLOCK];
+    for (j = 0; j < BLOCK; j++){
+        sum = 0.0;
+        for (i = 0; i < K; i++){
+            sum += A_p[row * K + i] * B_p[i * N + col + j];
+            // if(row == 0 && col == 0 && j == 161 && i < 20)
+            //     printf("PTHREAD %f %d %f\n",sum, i, A_p[row * K + i] * B_p[i * N + col]);
+        }
+        p[j] = sum;
+    }
+    free(arg);
+    pthread_exit(p);
+}
 
-    //         // for(int mid = 0; mid < K; mid++){
-    //         //     C[j + i*N] += A[mid + i*K] * B[mid + j*K];
-    //         // }
-    //         C[j + i*N] = sum;
-    //     }
-    // }
-    // int size = 0; 
-    // for (int i = 0; i < M; i++) 
-    //     for (int j = 0; j < K; j++) 
-    //         if (A[i*K + j] != 0.0) 
-    //             size++; 
+void conv2d(double *C, double *A, double *B, int M_p, int K_p, int N_p){
+    M = M_p; N = N_p; K = K_p;
+    A_p = (double *)malloc(M*K*sizeof(double));
+    B_p = (double *)malloc(N*K*sizeof(double));
+    int size = M*N;
+
+    int i, j;
   
-    // double compactMatrixA[size]; 
-    // int compact[2][size];
-  
-    // int k = 0; 
-    // for (int i = 0; i < M; i++) 
-    //     for (int j = 0; j < K; j++) 
-    //         if (A[i*K + j] != 0.0) 
-    //         { 
-    //             compact[0][k] = i; 
-    //             compact[1][k] = j; 
-    //             compactMatrixA[k] = A[i*K + j] ; 
-    //             k++; 
-    //         } 
+    for(i = 0; i < M*K; i++) A_p[i] = A[i];
+    for(i = 0; i < N*K; i++) B_p[i] = B[i];
 
-    // size = 0; 
-    // for (int i = 0; i < K; i++) 
-    //     for (int j = 0; j < N; j++) 
-    //         if (B[i*K + j] != 0) 
-    //             size++; 
-  
-    // // for (int i = 0; i < M; i++) {
-    // int b_offset;
-    // int row, col;
-    // double val;
-    // for (int j = 0; j < N; j++) {
-    //     b_offset = j * K;
-    //     for(int i = 0; i<size; i++){
-    //         row = compact[0][i];
-    //         col = compact[1][i];
-    //         val = compactMatrixA[i];
-    //         C[row * N + j] += val*B[col*N + j];
-    //     }
-    // }
-    // }
-    // int compactMatrixB[3][size]; 
-  
-    // k = 0; 
-    // for (int i = 0; i < M; i++) 
-    //     for (int j = 0; j < K; j++) 
-    //         if (B[i*K + j] != 0) 
-    //         { 
-    //             compactMatrixB[0][k] = i; 
-    //             compactMatrixB[1][k] = j; 
-    //             compactMatrixB[2][k] = B[i*K + j] ; 
-    //             k++; 
-    //         } 
-    
+    pthread_t *threads;
+    if(size < 30000){
+        threads = (pthread_t*)malloc(size*sizeof(pthread_t));
 
-    for (int i = 0; i < M; i++){
-        for (int j = 0; j < N; j++){
-            double sum = 0.0;
-            for (int k = 0; k < K; k++) {
-                sum += A[i*K + k] * B[k * N + j];
+        for (i = 0; i < M; i++)
+            for (j = 0; j < N; j++)  {  
+                int *data;
+                data = calloc(2,sizeof(int));
+                *data = i;
+                *(data + 1) = j;
+                if(pthread_create(&threads[i * N + j], NULL, mult, (void*)data) != 0){
+                    printf("Create failed at %d %d\n", i, j);
+                    exit(-1);
+                    return;
+                }
+
             }
-            C[i * N + j] = sum;
+        for (i = 0; i < size; i++)  
+        {
+            void *k;
+            pthread_join(threads[i], &k);
+            C[i] = *(double *)k;
+        }
+        for (int i = 0; i < M; i++){
+            for (int j = 0; j < N; j++){
+                double sum = 0.0;
+                for (int k = 0; k < K; k++) {
+                    sum += A[i*K + k] * B[k * N + j];
+                    if(i == 0 && j == 29999)
+                        printf("NORMAL %f %d %f\n",sum, k, A[i*K + k] * B[k * N + j]);
+                }
+                if (C[i * N + j] - sum > 0.1){
+                    printf("FAILED at C[%d][%d]: %.3f is not equal to %.3f\n", i, j, sum, C[i * N + j]);
+                    exit(-1);
+                    return;
+                }
+            
+            }
         }
     }
+    else{
+        int NUM_THR = size / BLOCK;
+        printf("threads %d\n", NUM_THR);
+        threads = (pthread_t*)malloc(NUM_THR*sizeof(pthread_t));
+        // printf("YUUUUP\n");
+        int counter = 0;
+        for (i = 0; i < M; i++){
+            for (j = 0; j < N; j = j + BLOCK){
+                int *data;
+                data = calloc(2,sizeof(int));
+                *data = i;
+                *(data + 1) = j;
+                if(pthread_create(&threads[counter++], NULL, mult2, (void*)data) != 0){
+                    printf("Create failed at %d %d\n", i, j);
+                    exit(-1);
+                    return;
+                }
+            }
+        }
+        double *res;
+        for (i = 0; i < NUM_THR; i++) {
+            // printf("DONE i = %d\n", i);
+            // double k[min(BLOCK, N - counter)];
+            void *k;
+            pthread_join(threads[i], &k);
+            res = k;
+            j = 0;
+            while(j < BLOCK){
+                // if(i == 0 && j >= 161)
+                //     printf("res %d is %f\n", j, res[j]);
+                C[i * BLOCK + j] = res[j];
+                j++;
+            }
+        }
+
+        for (int i = 0; i < M; i++){
+            for (int j = 0; j < N; j++){
+                double sum = 0.0;
+                for (int k = 0; k < K; k++) {
+                    sum += A[i*K + k] * B[k * N + j];
+                    // if(i == 0 && j == 161 && k < 20)
+                    //     printf("NORMAL %f %d %f\n",sum, k, A[i*K + k] * B[k * N + j]);
+                }
+                if (C[i * N + j] - sum > 0.1){
+                    printf("FAILED at C[%d][%d]: %f is not equal to %.3f\n", i, j, sum, C[i * N + j]);
+                    exit(-1);
+                    return;
+                }
+            }
+        }
+
+    }
+    
+    free(A_p);
+    free(B_p);
 }
--- src/dnn_avx.py
+++ src/dnn_avx.py
@@ -178,8 +178,10 @@ class Conv2D(DnnNode):
         output_height  = int(((in_height - filter_height + self.pad_h) / self.strides[1]) + 1)
         output_width   = int(((in_width - filter_width + self.pad_w) / self.strides[2]) + 1)
         # self.prev_res = np.zeros((batch, in_height + self.pad_h, in_width + self.pad_w, in_channels))
-        self.result = np.zeros((batch, output_height, output_width, out_channels))
-        self.out_shape = self.result.shape
+        # self.result = np.zeros((batch, output_height, output_width, out_channels))
+        self.result = None
+        # self.out_shape = self.result.shape
+        self.out_shape = (batch, output_height, output_width, out_channels)
         self.filter_height, self.filter_width, self.in_channels, out_channels = kernel.shape
 
     def run(self):
@@ -198,24 +200,33 @@ class Conv2D(DnnNode):
         X_col = im2col_indices(X, h_filter, w_filter, padding, stride=self.strides[1])
         W_col = W.reshape(n_filters, -1)
 
-        A = W_col.astype(c_double)
-        B = X_col.astype(c_double)
         m, n = W_col.shape
         n1, k = X_col.shape
-        C = np.zeros((m, k)).astype(c_double)
 
-        assert n1==n, "Shapes do not match"
+        if self.name[-1] in"78":
+            print(W_col.shape, X_col.shape)
+            m, n = W_col.shape
+            n1, k = X_col.shape
+            assert n1==n, "Shapes do not match"
 
-        func = mylib.conv2d
-        func.argtypes = [POINTER(c_double), POINTER(c_double), POINTER(c_double),
-                        c_size_t, c_size_t, c_size_t]
-        A_p = A.ctypes.data_as(POINTER(c_double))
-        B_p = B.ctypes.data_as(POINTER(c_double))
-        C_p = C.ctypes.data_as(POINTER(c_double))
-        func(C_p, A_p, B_p, m, n, k)
-        out = C.astype("float64")
+            A = W_col.astype(c_double)
+            A_p = A.ctypes.data_as(POINTER(c_double))
+
+            func = mylib.conv2d
+            func.argtypes = [POINTER(c_double), POINTER(c_double), POINTER(c_double),
+                                c_size_t, c_size_t, c_size_t]
+
+            B = X_col.astype(c_double)
+            C = np.zeros((m, k)).astype(c_double)
+            B_p = B.ctypes.data_as(POINTER(c_double))
+            C_p = C.ctypes.data_as(POINTER(c_double))
+            func(C_p, A_p, B_p, m, n, k)
+            out = C.astype("float64")
+        else:
+            out = W_col @ X_col
 
         # out = W_col @ X_col
+        # print(out[0][:10])
         print(out.shape)
         out = out.reshape(n_filters, h_out, w_out, n_x)
         out = out.transpose(3, 1, 2, 0)
